3d1f903eb4a91420d60177fd9b1a358a0778531d
Umair Riaz <umair.riaz@racert.com>
2026-03-30 11:00:42 +0500

    change components path

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @vite(['resources/js/app.jsx', "resources/js/Components/{$page['component']}.jsx"])
        
     </head>
     <body class="font-sans antialiased">
